2b65933eb02a9ba821138f4e385649874cba7e1f
Nathan Dias <astfdatamining@gmail.com>
2022-07-28 19:46:04 -0300

    Bug testing

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+
+const prefix = '!';
+
+const client = new Discord.Client({
+    allowedMentions: {
+        parse: [`users`, `roles`],
+        repliedUser: true,
+
+    },
+    intents: [
+        "Guilds",,
+        "GuildMessages",
+        "GuildPresences",
+        "GuildMembers",
+        "GuildMessageReactions",
+    ],
+});
+
+client.on("ready", () => {
+    console.log("Bot on krl!")
+})
+
+client.on('messageCreate', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping'){
+        message.channel.send('Pong!')
+    }
+})
+
+client.login("MTAwMTYzMzM5MzA3MjY4NTEyOA.GMMGCM.T4wj4rD-HMtPPitCGX_0DuRtxhmCt-NmliJAxs")
