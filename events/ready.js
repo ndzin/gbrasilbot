ca955bd145f40ae2b109865de9b2d1668690cc7d
Yuuto <82049175+onlyKuroii@users.noreply.github.com>
2022-09-07 22:17:08 -0300

    Eventos movidos para a pasta apropriada

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,11 @@
+const { ActivityType } = require("discord.js")
 
 module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-		console.log(`Bot ON! Iniciado como ${client.user.tag}`);
-	},
-};
+  name: "ready",
+  once: true,
+  execute (client) {
+    console.log(`Bot Ligado Como ${client.user.tag}`)
+    client.user.setActivity(`você digitar /help ! 💕`, { type: ActivityType.Watching })
+    client.user.setStatus("online")
+  },
+}
